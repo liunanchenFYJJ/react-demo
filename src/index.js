5774f36ce205c80cac649f5b849e73c05f75f435
liunanchenFYJJ <liunan_chen@163.com>
2018-01-22 23:34:26 +0800

    elements

--- src/index.js
+++ src/index.js
@@ -18,7 +18,43 @@ import './index.css';
 //   }
 // }
 
+// JSX
+const user = {
+  firstName: 'jay',
+  lastName: 'chow'
+}
+function formatName (user) {
+  return user.firstName + ' ' + user.lastName
+}
+
+function getGreetings (user) {
+  if (user) {
+    return <h1>hello, { formatName(user) }</h1>
+  } else {
+    // const user = null    
+    return <h1>hello, stranger!</h1>
+  }
+}
+
+// element 一旦创建，不可改变！唯有创建新的
+function tick () {
+  const element = (
+    <div>
+      <h1>hello, element</h1>
+      <h2>time is {''} { new Date().toLocaleTimeString() }</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
+}
+const element = <h1>{ getGreetings(user) }</h1>
+
 ReactDOM.render(
-  <h1>hello React</h1>,
+  // <h1>hello React</h1>,
+  element,
+  // a “root” DOM
   document.getElementById('root')
 )
+setInterval(tick, 1000)
