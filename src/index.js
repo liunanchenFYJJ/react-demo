b643ecfbe329cd2b89bdd6d120f0d4e289562ba6
liunanchenFYJJ <liunan_chen@163.com>
2018-01-22 19:19:05 +0800

    second commit

--- src/index.js
+++ src/index.js
@@ -2,17 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class ShoppingList extends React.component {
-  render () {
-    return (
-      <div className="shopping-list">
-        <h1>Shopping List for { this.props.name }</h1>
-        <ul>
-          <li>Instagram</li>
-          <li>WhatsApp</li>
-          <li>QQ</li>          
-        </ul>
-      </div>
-    )
-  }
-}
+// render return 的是需要渲染的
+// class ShoppingList extends React.component {
+//   render () {
+//     return (
+//       <div className="shopping-list">
+//         <h1>Shopping List for { this.props.name }</h1>
+//         <ul>
+//           <li>Instagram</li>
+//           <li>WhatsApp</li>
+//           <li>QQ</li>          
+//         </ul>
+//       </div>
+//     )
+//   }
+// }
+
+ReactDOM.render(
+  <h1>hello React</h1>,
+  document.getElementById('root')
+)
