94439f8314ae82e6929f77fbaf5cf64208d6701d
liunanchenFYJJ <liunan_chen@163.com>
2018-01-23 23:58:32 +0800

    class way to write components

--- src/index.js
+++ src/index.js
@@ -25,25 +25,31 @@ const user = {
 }
 
 // 写法一
-// class formatName extends React.component {
-//   render () {
-//     return this.user.firstName + ' ' + this.user.lastName
-//   }
-// }
+class formatName extends React.Component {
+  render () {
+    return this.props.firstName + ' ' + this.props.lastName
+  }
+}
 
 // 写法二
-function formatName (user) {
-  return user.firstName + ' ' + user.lastName
-}
+// function formatName (user) {
+//   return user.firstName + ' ' + user.lastName
+// }
 // component 组件名、组件标签大写； 小写默认为HTML标签
-function F1(props) {
-  return <h2>hello, { props.name }.age is { props.age === '' ? 'empty' : props.age }</h2>
+// function F1(props) {
+//   return <h2>hello, { props.name }.age is { props.age === '' ? 'empty' : props.age }</h2>
+// }
+
+class F1 extends React.Component {
+  render () {
+    return <h2>hello, { this.props.name }.age is { this.props.age === '' ? 'empty' : this.props.age }</h2>
+  }
 }
 // 组件复用
 function App () {
   return (
     <div>
-      <F1 name="martin"/>
+      <F1 name={user.lastName}/>
       <F1 name="martin1" age="78"/>
       <F1 name="martin2"/>    
     </div>
@@ -65,19 +71,20 @@ function formatDate (date) {
 }
 
 // 以下给<Comment /> 定义了 author{}、text、 date 三个属性，其中author 为对象
+// function方式写component
 // 拆分
 // 注意：All React components must act like pure functions with respect to their props
-function Avatar (props) {
-  return (
-    <img className="Avatar" src={ props.author.avatarUrl } alt={ props.author.name }/>
-  )
-}
-// class方式写component？
-// class Avatar extends React.component {
-//   render (props) {
-//     return <img className="Avatar" src={ props.author.avatarUrl } alt={ props.author.name }/>
-//   } 
+// function Avatar (props) {
+//   return (
+//     <img className="Avatar" src={ props.author.avatarUrl } alt={ props.author.name }/>
+//   )
 // }
+// class方式写component
+class Avatar extends React.Component {
+  render () {
+    return <img className="Avatar" src={ this.props.author.avatarUrl } alt={ this.props.author.name }/>
+  } 
+}
 function Username (props) {
   return (
     <div className="Username">{ props.author.name }</div>    
@@ -132,18 +139,18 @@ function getGreetings (user) {
 }
 
 // element 一旦创建，不可改变！唯有创建新的
-// function tick () {
-//   const element = (
-//     <div>
-//       <h1>hello, element</h1>
-//       <h2>time is {''} { new Date().toLocaleTimeString() }</h2>
-//     </div>
-//   );
-//   ReactDOM.render(
-//     element,
-//     document.getElementById('root')
-//   )
-// }
+function tick () {
+  const element = (
+    <div>
+      <h1>hello, element</h1>
+      <h2>time is {''} { new Date().toLocaleTimeString() }</h2>
+    </div>
+  );
+  ReactDOM.render(
+    element,
+    document.getElementById('root')
+  )
+}
 // const element = <h1>{ getGreetings(user) }</h1>
 // const element = <Welcome name="Sara" /> 也可以是用户自定义的
 
@@ -154,5 +161,5 @@ ReactDOM.render(
   // a “root” DOM
   document.getElementById('root')
 )
-// setInterval(tick, 1000)
+setInterval(tick, 1000)
 // setTimeout(tick, 1000);
