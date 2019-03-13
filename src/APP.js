de62e428f2412334229a38410eaaa853894109d4
LiunanChen <liunan_chen@163.com>
2019-03-13 23:34:47 +0800

    start component

--- src/APP.js
+++ src/APP.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import Mycom from './components/Mycom'
 
 export default class App extends Component {
     render() {
-        return (<div>hello react</div>);
+        return <div>
+            hello react
+            <Mycom />
+            </div>;
     }
 }
---
+++ src/components/Mycom.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Mycom extends Component {
+    render() {
+        return <span className="mycom">my Component</span>
+    }
+}
